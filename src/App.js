21bc2a2b61d989d599fadc38f24bb2e83f16fef2
sw.tang <15090804865@163.com>
2020-10-18 15:31:55 +0800

    [首页】标题修改

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          SugerKid：这是正常发布的页面。
         </p>
         <a
           className="App-link"
