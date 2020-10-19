1367ed7ef6efce410417a1cb700d04a9060ec7a0
sw.tang <15090804865@163.com>
2020-10-20 01:35:22 +0800

    【首页】修改灰度发布的标题

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          SugerKid：这是正常发布的页面。
+          SugerKid：这是灰度发布的页面。
         </p>
         <a
           className="App-link"
